5b5511810f14cdfa5ecb5582690b0eef346112f7
dajenai <73131602+dajenai@users.noreply.github.com>
2020-11-23 23:07:35 -0500

    Form & Search Engine added

--- src/app.js
+++ src/app.js
@@ -43,8 +43,19 @@ function displayTemperature(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+}
+function search(city) {
+  let apiKey = "edd438239dd8dda35a73802dcb584988";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
 }
 
-let apiKey = "edd438239dd8dda35a73802dcb584988";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Brooklyn&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+search("Brooklyn");
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
